eb9eb177124067148de8d6b6522ab977a08401d8
Anurag7667 <92031232+Anurag7667@users.noreply.github.com>
2022-01-10 09:32:26 +0530

    Update Solution2.java

--- Goldman_sachs/Solution2.java
+++ Goldman_sachs/Solution2.java
@@ -34,7 +34,7 @@ class GFG {
 
 // User function Template for Java
 
-class Solution {
+class Solution2 {
     int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
         // code here
              if(L2[0]>R1[0]||L1[0]>R2[0]||L1[1]<R2[1]||L2[1]<R1[1])
